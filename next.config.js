33a4feabe4a799edd6d0c5e05682a49b72fb4999
Sohail Barat <barat.sohail@gmail.com>
2025-08-20 20:59:50 -0600

    Fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: false, // Let Vercel optimize images
+  },
 };
 
 module.exports = nextConfig;
